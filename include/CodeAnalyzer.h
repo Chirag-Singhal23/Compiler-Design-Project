cc841c59cbe1ebd0db59375edceaeb24242b10bd
adityamodi-07 <aditya3107modi@gmail.com>
2025-05-18 22:25:13 +0530

    Add files via upload

---
+++ include/CodeAnalyzer.h
@@ -0,0 +1,15 @@
+#ifndef CODE_ANALYZER_H
+#define CODE_ANALYZER_H
+
+#include "Parser.h"
+
+class CodeAnalyzer {
+public:
+    void analyze(const std::shared_ptr<ASTNode>& root);
+
+private:
+    void checkRedundantConditions(const std::shared_ptr<ASTNode>& node);
+    void report(const std::string& message);
+};
+
+#endif
